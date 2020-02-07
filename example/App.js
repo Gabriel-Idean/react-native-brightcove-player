969de115858da4fc46bf68f8da5bf83d561b4acd
Matej Drobnic <matej.drobnic@nzme.co.nz>
2020-02-07 16:31:11 +1300

    resting NZH ads

--- example/App.js
+++ example/App.js
@@ -1,157 +1,34 @@
-import React, { Component } from 'react';
-import { FlatList, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { BrightcovePlayer, BrightcovePlayerPoster, BrightcovePlayerUtil } from 'react-native-brightcove-player';
-
-const ACCOUNT_ID = '5434391461001';
-const POLICY_KEY =
-  'BCpkADawqM0T8lW3nMChuAbrcunBBHmh4YkNl5e6ZrKQwPiK_Y83RAOF4DP5tyBF_ONBVgrEjqW6fbV0nKRuHvjRU3E8jdT9WMTOXfJODoPML6NUDCYTwTHxtNlr5YdyGYaCPLhMUZ3Xu61L';
-const PLAYLIST_REF_ID = 'brightcove-native-sdk-plist';
+import React, {Component} from 'react';
+import {StatusBar, StyleSheet, View} from 'react-native';
+import {BrightcovePlayerIMA} from 'react-native-brightcove-player';
 
 export default class App extends Component {
-  state = {
-    videos: [],
-    offlineVideos: [],
-    playback: {
-      referenceId: null,
-      videoToken: null
-    }
-  };
-
-  componentDidMount() {
-    BrightcovePlayerUtil.getPlaylistWithReferenceId(
-      ACCOUNT_ID,
-      POLICY_KEY,
-      PLAYLIST_REF_ID
-    )
-      .then(videos => {
-        this.setState({
-          videos
-        });
-      })
-      .catch(console.warn);
-    BrightcovePlayerUtil.getOfflineVideoStatuses(ACCOUNT_ID, POLICY_KEY)
-      .then(offlineVideos => {
-        this.setState({
-          offlineVideos
-        });
-      })
-      .catch(console.warn);
-    this.disposer = BrightcovePlayerUtil.addOfflineNotificationListener(
-      offlineVideos => {
-        this.setState({
-          offlineVideos
-        });
-      }
-    );
-  }
-
-  requestDownload(videoId) {
-    BrightcovePlayerUtil.requestDownloadVideoWithVideoId(
-      ACCOUNT_ID,
-      POLICY_KEY,
-      videoId
-    ).catch(() => {});
-  }
-
-  play(item) {
-    const downloadStatus = this.state.offlineVideos.find(
-      video => video.videoId === item.videoId
-    );
-    this.setState({
-      playback:
-        downloadStatus && downloadStatus.downloadProgress === 1
-          ? {
-              videoToken: downloadStatus.videoToken
-            }
-          : {
-              referenceId: item.referenceId
-            }
-    });
-  }
-
-  delete(videoToken) {
-    BrightcovePlayerUtil.deleteOfflineVideo(
-      ACCOUNT_ID,
-      POLICY_KEY,
-      videoToken
-    ).catch(console.warn);
-  }
-
-  componentWillUnmount() {
-    this.disposer && this.disposer();
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
-        <BrightcovePlayer
-          style={styles.video}
-          accountId={ACCOUNT_ID}
-          policyKey={POLICY_KEY}
-          autoPlay
-          {...this.state.playback}
-        />
-        <FlatList
-          style={styles.list}
-          extraData={this.state.offlineVideos}
-          data={this.state.videos}
-          keyExtractor={item => item.referenceId}
-          renderItem={({ item }) => {
-            const downloadStatus = this.state.offlineVideos.find(
-              video => video.videoId === item.videoId
-            );
-            return (
-              <View style={styles.listItem}>
-                <TouchableOpacity
-                  style={styles.mainButton}
-                  onPress={() => this.play(item)}
-                >
-                  <BrightcovePlayerPoster
-                    style={styles.poster}
-                    accountId={ACCOUNT_ID}
-                    policyKey={POLICY_KEY}
-                    referenceId={item.referenceId}
-                  />
-                  <View style={styles.body}>
-                    <Text style={styles.name}>{item.name}</Text>
-                    <Text>{item.description}</Text>
-                    {downloadStatus ? (
-                      <Text style={styles.offlineBanner}>
-                        {downloadStatus.downloadProgress === 1
-                          ? 'OFFLINE PLAYBACK'
-                          : `DOWNLOADING: ${Math.floor(
-                              downloadStatus.downloadProgress * 100
-                            )}%`}
-                      </Text>
-                    ) : null}
-                    <Text style={styles.duration}>
-                      {`0${Math.floor(item.duration / 60000) % 60}`.substr(-2)}:
-                      {`0${Math.floor(item.duration / 1000) % 60}`.substr(-2)}
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.downloadButton}
-                  onPress={() => {
-                    if (!downloadStatus) {
-                      this.requestDownload(item.videoId);
-                    } else {
-                      this.delete(downloadStatus.videoToken);
-                    }
-                  }}
-                >
-                  <Text>
-                    {!downloadStatus
-                      ? '💾'
-                      : downloadStatus.downloadProgress === 1
-                      ? '🗑'
-                      : '⏳'}
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            );
+        <BrightcovePlayerIMA
+          ref={ref => {
+            this.bcPlayer = ref;
           }}
+          onEnterFullscreen={this.onEnterFullscreen}
+          onExitFullscreen={this.onExitFullscreen}
+          accountId={1308227299001}
+          policyKey={
+            'BCpkADawqM2y6-Am7ImPI7GefImBwWww7W4WC53cN-Dp5yDuTTYic0SE7zOciUar3GPtzRZVmFaHi7Y_lw_pk-1he4JMbkKGVl9LBmIDLKafgXAesc8itDvIJbkSLdx4Kx6H9V_noezGNEx-'
+          }
+          videoId={6129374380001}
+          onPause={this.onPause}
+          onPlay={this.onPlay}
+          onEnd={this.onEnd}
+          settings={{
+            IMAUrl:
+              'https://pubads.g.doubleclick.net/gampad/ads?iu=/83069739/jeff/sport&description_url=http%3A%2F%2Fnzme.co.nz&env=vp&impl=s&correlator=&tfcd=0&npa=0&gdfp_req=1&output=vast&sz=620x350&cust_params=mb%3D0433802%26sa2%3D132700%26av%3D1.0%26pt%3Dvideo%26content_type%3Dfree%26subscriber%3Dfalse%26kw%3Dfocus%2Csport%2Cwhen%2Cpeople%2Ctalk%2Cabout%2Crugby%2Cthey%2Czealand%26oid%3Dg216540%26content_topics%3DSportVideo%2CVideo%26syndicator%3DNZH%3A%26arc_uuid%3D0&unviewed_position_start=1',
+            autoAdvance: false,
+            autoPlay: true, // initial autoPlay prop
+            allowsExternalPlayback: true,
+          }}
+          autoPlay={true}
         />
       </View>
     );
@@ -161,32 +38,32 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
   video: {
     width: '100%',
-    height: 260
+    height: 260,
   },
   list: {
-    flex: 1
+    flex: 1,
   },
   listItem: {
     flexDirection: 'row',
     borderBottomWidth: 1,
-    borderBottomColor: 'lightgray'
+    borderBottomColor: 'lightgray',
   },
   mainButton: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   body: {
     flex: 1,
     padding: 10,
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
   name: {
     fontSize: 14,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   offlineBanner: {
     fontSize: 10,
@@ -194,20 +71,20 @@ const styles = StyleSheet.create({
     color: 'white',
     alignSelf: 'flex-start',
     padding: 3,
-    backgroundColor: 'deepskyblue'
+    backgroundColor: 'deepskyblue',
   },
   duration: {
     marginTop: 'auto',
-    opacity: 0.5
+    opacity: 0.5,
   },
   poster: {
     width: 100,
     height: 100,
-    backgroundColor: 'black'
+    backgroundColor: 'black',
   },
   downloadButton: {
     padding: 16,
     marginLeft: 'auto',
-    alignSelf: 'center'
-  }
+    alignSelf: 'center',
+  },
 });
